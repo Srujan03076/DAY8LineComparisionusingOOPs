be9fc994a9e48d77f5c21d7ce9381a4b45182e4e
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-12 14:17:57 +0530

    Adding Line Comoarision UC 2 Program

---
+++ LIneComparision2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace LineCompUsingOOPS
+{
+    public class UC_2
+    {
+        public static void CheckEquality()
+        {
+            Console.WriteLine("Enter co-ordinates of line1");
+            Console.WriteLine("Enter co-ordinates (x1, y1): ");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter co-ordinates (x2, y2): ");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter co-ordinates of line2");
+            Console.WriteLine("Enter co-ordinates (a1, b1): ");
+            double a1 = double.Parse(Console.ReadLine());
+            double b1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter co-ordinates (a2, b2): ");
+            double a2 = double.Parse(Console.ReadLine());
+            double b2 = double.Parse(Console.ReadLine());
+
+
+            double length1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double length2 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
+
+            //Console.WriteLine("L1 "+length1+ " L2 "+length2 );
+
+            string answer1 = Convert.ToString(length1);
+            string answer2 = Convert.ToString(length2);
+
+            Console.WriteLine(answer1.Equals(answer2));
+        }
+    }
+
+}
